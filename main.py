3c156ba8ad4c56c6d8ad08d4d68cde9bbbc0fc63
malanyaonjustine029-lgtm <malanyaonjustine029@gmail.com>
2026-01-09 23:30:19 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+{
+  "predictions": [
+    {
+      "x": 208.5,
+      "y": 280.5,
+      "width": 117,
+      "height": 409,
+      "confidence": 0.932,
+      "class": "plastic bottle",
+      "class_id": 1,
+      "detection_id": "c51a4188-9130-4b3b-9999-51d317c23d6c"
+    }
+  ]
+}
